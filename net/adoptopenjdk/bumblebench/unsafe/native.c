2ac81fd930985cc83e117b2da18bca1e591cc588
Spencer Comin <spencer.comin@ibm.com>
2024-07-23 09:02:15 -0400

    Debug printing

--- net/adoptopenjdk/bumblebench/unsafe/native.c
+++ net/adoptopenjdk/bumblebench/unsafe/native.c
@@ -1,14 +1,20 @@
 #include <jni.h>
+#include <stdint.h>
+#include <stdio.h>
 
-static long obj1;
-static long obj2;
+static uint8_t obj1[16];
+static uint8_t obj2[16];
 
 JNIEXPORT jlong JNICALL
 Java_net_adoptopenjdk_bumblebench_unsafe_Native_getOffset1(JNIEnv *env, jclass cls) {
-	return (jlong)(&obj1);
+	uint8_t *ptr = obj1;
+	printf("From C: 16B allocated at 0x%p\n", ptr);
+	return (jlong)(ptr);
 }
 
 JNIEXPORT jlong JNICALL
 Java_net_adoptopenjdk_bumblebench_unsafe_Native_getOffset2(JNIEnv *env, jclass cls) {
-	return (jlong)(&obj2);
+	uint8_t *ptr = obj2;
+	printf("From C: 16B allocated at 0x%p\n", ptr);
+	return (jlong)(ptr);
 }
