1273fa49eafa07a0e4ed2accbb01ef54d9723c8a
Spencer Comin <spencer.comin@ibm.com>
2024-11-12 14:59:46 -0500

    Add benchmarks for compress and expand
    This commit adds benchmarks for the following methods:
    
    - Integer.compress(int, int)
    - Integer.expand(int, int)
    - Long.compress(long, long)
    - Long.expand(long, long)
    
    Signed-off-by: Spencer Comin <spencer.comin@ibm.com>

---
+++ net/adoptopenjdk/bumblebench/bits/IntegerCompressBench.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+package net.adoptopenjdk.bumblebench.bits;
+
+import java.util.Random;
+
+import net.adoptopenjdk.bumblebench.core.MicroBench;
+
+public final class IntegerCompressBench extends MicroBench {
+    static int a;
+
+    protected long doBatch(long numIterations) {
+        a = (int) numIterations;
+        for (long i = numIterations; i >= 0; i--) {
+            a += Integer.compress(Integer.compress(a, (int)i), Integer.compress((int)i, a));
+        }
+        return numIterations;
+    }
+}
